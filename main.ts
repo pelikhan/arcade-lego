27af3e6c40c466985d777270cff66555972dd4dc
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-11-29 01:35:16 -0800

    created blocks

--- main.ts
+++ main.ts
@@ -1,45 +1,66 @@
-scene.setBackgroundColor(7)
-const i = img`
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . 1 5 5 1 . . .
-    . . . . . . 1 1 1 1 1 1 . . . .
-    . . . . . 1 1 5 5 5 5 5 1 . . .
-    . 1 1 1 1 1 5 5 5 5 5 5 5 1 . .
-    . 1 2 5 1 5 5 5 5 5 5 5 5 1 . .
-    . . 1 5 5 1 5 2 1 f 5 2 2 f . .
-    . . 1 2 5 5 1 1 f f 5 2 2 c . .
-    1 1 2 1 5 5 5 2 f 2 2 2 2 2 1 .
-    1 2 2 c 2 5 5 1 5 2 2 2 2 2 2 1
-    8 2 2 2 c c 1 5 5 5 5 5 5 5 1 .
-    8 1 2 2 2 2 2 5 5 5 5 5 5 5 1 .
-    . 8 2 2 2 2 2 2 5 5 5 5 5 2 1 .
-    . . 8 1 2 2 2 2 2 5 5 5 1 1 . .
-    . . . 8 8 8 8 8 8 8 8 1 1 . . .
-`;
-
-const cell = 9;
-const cellmargin = 2;
-const celldot = 4;
-const lego = image.create(i.width * cell, i.height * cell)
-for(let x = 0; x < i.width; ++x) {
-    for (let y = 0; y < i.height; ++y) {
-        const c = i.getPixel(x, y);
-        if (!c) continue;
-        // paint lego image
-        const lx = x * (cell - 1);
-        const ly = y * (cell - 1);
-        // inside        
-        lego.fillRect(lx, ly, cell, cell, c)
-        // outline
-        lego.drawRect(lx, ly, cell, cell, 0xf)
-        // dots
-        const cdot = (c + 1) % 0xf
-        lego.setPixel(lx + cellmargin, ly + cellmargin,  cdot)
-        lego.setPixel(lx + cellmargin, ly + cellmargin + celldot,  cdot)
-        lego.setPixel(lx + cellmargin + celldot, ly + cellmargin,  cdot)
-        lego.setPixel(lx + cellmargin + celldot, ly + cellmargin + celldot,  cdot)
+/**
+ * Utilitities to make LEGO images
+ */
+//% icon="\uf24d"
+namespace lego {
+    /**
+     * Sets the default LEGO moulding colors
+     */
+    //% blockId=legosetpalette block="lego set moulding colors palette"
+    export function setMouldingColorsPalette() {
+        const buffer = color.ColorBuffer.fromBuffer(
+            hex`
+        000000
+        ffffff
+        ff0000
+        EE9DC3
+        ff6600
+        F7CE46
+        478CC6
+        00cc00
+        0000ff
+        68c3e2
+        990066
+        a06eb9
+        cda4de
+        F5C189
+        A83D15
+        000000
+            `
+            , color.ColorBufferLayout.RGB);
+        palette.setColors(buffer)
     }
-}
 
-let mySprite = sprites.create(lego)
+    /**
+     * Expands an image into a LEGO rendering
+     */
+    //% blockId=legolegoify block="legoify $i"
+    export function legoify(i: Image) {
+        const cell = 9;
+        const cellmargin = 2;
+        const celldot = 4;
+        const l = image.create(i.width * cell, i.height * cell)
+        for(let x = 0; x < i.width; ++x) {
+            for (let y = 0; y < i.height; ++y) {
+                const c = i.getPixel(x, y);
+                if (!c) continue;
+                // paint lego image
+                const lx = x * (cell - 1);
+                const ly = y * (cell - 1);
+                // inside        
+                l.fillRect(lx, ly, cell, cell, c)
+                // outline
+                l.drawRect(lx, ly, cell, cell, 0xf)
+                // dots
+                const cdot = (c + 1) % 0xf
+                l.setPixel(lx + cellmargin, ly + cellmargin,  cdot)
+                l.setPixel(lx + cellmargin, ly + cellmargin + celldot,  cdot)
+                l.setPixel(lx + cellmargin + celldot, ly + cellmargin,  cdot)
+                l.setPixel(lx + cellmargin + celldot, ly + cellmargin + celldot,  cdot)
+            }
+        }    
+        return l;    
+    }
+
+    setMouldingColorsPalette();
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,22 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+const i = img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . 1 5 5 1 . . .
+    . . . . . . 1 1 1 1 1 1 . . . .
+    . . . . . 1 1 5 5 5 5 5 1 . . .
+    . 1 1 1 1 1 5 5 5 5 5 5 5 1 . .
+    . 1 2 5 1 5 5 5 5 5 5 5 5 1 . .
+    . . 1 5 5 1 5 2 1 f 5 2 2 f . .
+    . . 1 2 5 5 1 1 f f 5 2 2 c . .
+    1 1 2 1 5 5 5 2 f 2 2 2 2 2 1 .
+    1 2 2 c 2 5 5 1 5 2 2 2 2 2 2 1
+    8 2 2 2 c c 1 5 5 5 5 5 5 5 1 .
+    8 1 2 2 2 2 2 5 5 5 5 5 5 5 1 .
+    . 8 2 2 2 2 2 2 5 5 5 5 5 2 1 .
+    . . 8 1 2 2 2 2 2 5 5 5 1 1 . .
+    . . . 8 8 8 8 8 8 8 8 1 1 . . .
+`;
+const l = lego.legoify(i)
+
+scene.setBackgroundColor(7)
+let mySprite = sprites.create(l)
